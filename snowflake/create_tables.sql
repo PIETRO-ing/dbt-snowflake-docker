e999630d5fd274abda84b63226237d23a7efd771
PIETRO-ing <pietro.passarella@proton.me>
2026-04-21 12:42:18 +0200

    created dbt_role

--- snowflake/create_tables.sql
+++ snowflake/create_tables.sql
@@ -48,4 +48,83 @@ file_format = (format_name = dbt_raw.public.csv_ff);
 
 select * from dbt_raw.public.dim_customer;
 
+create or replace table dbt_raw.public.dim_date(
+    date_sk number,
+    date date,
+    day number,
+    month number,
+    month_name varchar,
+    quarter number,
+    year number,
+    day_of_week number,
+    day_name varchar,
+    is_weekend boolean,
+    is_month_end boolean,
+    is_month_start boolean,
+    is_quarter_end boolean,
+    is_quarter_start boolean
+);
+
+list @dbt_raw.public.my_stage;
+
+copy into dbt_raw.public.dim_date
+from @dbt_raw.public.my_stage
+files = ('dim_date.csv')
+file_format =  (format_name = dbt_raw.public.csv_ff);
+
+create or replace table dbt_raw.public.dim_store(
+    store_sk number,
+    store_code varchar,
+    store_name varchar,
+    city varchar,
+    state_province varchar,
+    region varchar,
+    country varchar,
+    open_date date,
+    sq_ft number
+);
+
+copy into dbt_raw.public.dim_store
+from @dbt_raw.public.my_stage
+files = ('dim_store.csv')
+file_format = (format_name = dbt_raw.public.csv_ff);
+
+create or replace table dbt_raw.public.fact_returns(
+    sales_id number,
+    date_sk number,
+    store_sk number,
+    product_sk number,
+    returned_qty number,
+    return_reason varchar,
+    refund_amount number
+);
+
+copy into dbt_raw.public.fact_returns
+from @dbt_raw.public.my_stage
+files = ('fact_returns.csv')
+file_format = (format_name = dbt_raw.public.csv_ff);
+
+select * from fact_sales;
+
+-- create dbt_role
+use role accountadmin;
+create role dbt_role;
+show grants on warehouse dbt_wh;
+grant usage on warehouse dbt_wh to role dbt_role;
+grant role dbt_role to user pbln;
+grant all on database dbt_db to role dbt_role;
+
+grant all on database dbt_raw to role dbt_role;
+GRANT ALL ON SCHEMA dbt_raw.public TO ROLE dbt_role;
+GRANT ALL ON SCHEMA dbt_db.dbt_schema TO ROLE dbt_role;
+      
+
+use role dbt_role;
+
+SHOW GRANTS ON SCHEMA dbt_raw.public;
+SHOW GRANTS ON SCHEMA dbt_db.public;
+
+
+
+
 
