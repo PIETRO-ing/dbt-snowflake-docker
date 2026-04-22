2e69b2583d3998ec7bc9edfe363a6425aea2d862
PIETRO-ing <pietro.passarella@proton.me>
2026-04-22 15:38:29 +0200

    new macro

---
+++ data_pipeline/macros/generate_schema.sql
@@ -0,0 +1,14 @@
+{% macro generate_schema_name(megamart_schema, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
