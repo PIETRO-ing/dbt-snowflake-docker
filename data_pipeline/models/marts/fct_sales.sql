683ec9bca967a350bcb43313edce037bbfcda927
PIETRO-ing <pietro.passarella@proton.me>
2026-04-30 16:22:13 +0000

    cleaned fct sales jinja macro

--- data_pipeline/models/marts/fct_sales.sql
+++ data_pipeline/models/marts/fct_sales.sql
@@ -1,14 +1,16 @@
 {% set cols = ['category', 'gender' ]%}
 
 select 
-        {% for col in cols %}
-            {{col}}{% if not loop.last %}, {% endif %}
-        {% endfor %},
+    {% for col in cols %}
+        {{col}}
+            {% if not loop.last %}, {% endif %}
+    {% endfor %},
     round(sum(gross_amount)) as total_sales
 from
     {{ ref('int_salesinfo') }} 
 group by 
     {% for col in cols %}
-        {{ col }}{% if not loop.last %}, {% endif %}
+        {{ col }}
+            {% if not loop.last %}, {% endif %}
     {% endfor %}
 order by total_sales desc
