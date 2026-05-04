16acf1043e64141cba5e25eb39cb2776853089e0
PIETRO-ing <pietro.passarella@proton.me>
2026-05-04 10:23:00 +0000

    incremental load implemented on stg_date

--- data_pipeline/models/staging/stg_date.sql
+++ data_pipeline/models/staging/stg_date.sql
@@ -1,4 +1,9 @@
+{{config(materialized='incremental')}}
+
 select 
     *
 from 
     {{ source('source', 'dim_date') }}
+{% if is_incremental() %}
+  where date > coalesce((select max(date) from {{ this }}), '1900-01-01')
+{% endif %}
